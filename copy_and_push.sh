9111e7696b426182dfbe9024fa20144a7c7919d1
jdramirezl <juliandavid.ramirez@mercadolibre.com.co>
2024-02-09 14:51:07 -0500

    new push

--- copy_and_push.sh
+++ copy_and_push.sh
@@ -5,22 +5,25 @@ zshrc="/Users/julianramire/.zshrc"
 zshfuncs="/Users/julianramire/dotfiles"
 ohmyzsh="/Users/julianramire/.oh-my-zsh"
 nvim="/Users/julianramire/.config/nvim"
+tmux="/Users/julianramire/.tmux.conf"
 repo_path="/Users/julianramire/gh_dotfiles"
 commit_message="Auto commit on $(date +'%Y-%m-%d')"
 
 # Function to copy files from directories to the repository
 copy_files() {
-    cp -r "$zshrc" "$repo_path"
-    cp -r "$zshfuncs"/* "$repo_path"
-    cp -r "$ohmyzsh"/* "$repo_path"
-    cp -r "$nvim"/* "$repo_path"
+    cp -r "$zshrc" "$repo_path/zsh/"
+    cp -r "$zshfuncs"/* "$repo_path/zsh/zshfuncs/"
+    cp -r "$ohmyzsh"/* "$repo_path/ohmyzsh/"
+    cp -r "$nvim"/* "$repo_path/nvim/"
+    cp -r "$tmux" "$repo_path/tmux/"
 }
 
 # Function to commit and push changes to the repository
 push_to_repo() {
     cd "$repo_path" || exit
     git add .
-    git commit -m "$commit_message"
+    PRE_COMMIT_ALLOW_NO_CONFIG=1 git commit -m "$commit_message"  
+    terminal-notifier -title '🚀 Dotfile Backup' -message 'Your dotfiles were pushed to the repo!' -open 'https://github.com/jdramirezl/dotfiles'
     git push origin master --force # Assuming 'master' is the branch name
 }
 
